a82225fb49af1d33335c32b749b1fe1ca2c87e46
Jai Jordan <jai@jaijordan.com>
2020-08-04 15:51:17 +0100

    Vegan widget as per figma UI but missing horizontal alignment

--- app/Widgets/RedditRVegan.php
+++ app/Widgets/RedditRVegan.php
@@ -4,7 +4,7 @@ namespace App\Widgets;
 
 class RedditRVegan extends AbstractRedditWidget
 {
-    protected $name = 'Hot r/Vegan Posts';
+    protected $name = 'r/Vegan on Reddit';
     protected $type = 'reddit-rvegan';
     protected $description = 'Displays threads from the vegan subreddit.';
 
@@ -12,6 +12,12 @@ class RedditRVegan extends AbstractRedditWidget
     protected $subreddit = 'vegan';
     protected $sortMode = 'hot';
     protected $numberOfThreads = '6';
+
+    // Temporarily TURN OFF caching during development.
+    // TODO: Remove me before merging.
+    public function getTtl(): int {
+        return 0;
+    }
 
     /**
      * If this returns true then the thread will be removed.
--- resources/js/widgets/RedditRVegan.vue
+++ resources/js/widgets/RedditRVegan.vue
@@ -1,7 +1,15 @@
 <template>
   <Widget :name="name">
     <p v-for="thread of data" :key="thread.permalink">
-      <a :href="thread.permalink" target="_blank">{{ thread.title }}</a>
+      <a :href="thread.permalink" target="_blank">
+        <img :src="thread.thumbnail" v-if="thread.thumbnail !== 'self'" height="140px" width="140px" />
+        <img :src="thread.thumbnail" v-if="thread.thumbnail == 'self'" height="140px" width="140px" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_173b9ddb486%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_173b9ddb486%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274%22%20y%3D%22104.5%22%3Eno_thumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+
+        <blockquote class="blockquote">
+          <p class="mb-0">{{ thread.title }}</p>
+          <footer class="blockquote-footer">{{ thread.ups }} upvotes | {{ thread.num_comments }} comments</footer>
+        </blockquote>
+      </a>
     </p>
   </Widget>
 </template>
@@ -31,3 +39,10 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+a, a:hover, a:visited {
+  color: inherit;
+  text-decoration: inherit;
+}
+</style>
