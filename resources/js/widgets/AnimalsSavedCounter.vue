afb96ec3977faec010c29883cb267adad9cd1f3a
bake <bake@192k.pw>
2020-08-21 17:13:00 +0200

    Added static resources information

--- resources/js/widgets/AnimalsSavedCounter.vue
+++ resources/js/widgets/AnimalsSavedCounter.vue
@@ -13,10 +13,17 @@
       </div>
     </label>
 
-    <ul class="list-group" v-for="animal in saved" :key="animal.item">
+    <ul class="list-group" v-for="animal in animals" :key="animal.item">
       <li class="list-group-item px-0 border-0 d-flex justify-content-between">
         <span>{{ animal.item }}</span>
         <span>{{ animal.value }}</span>
+      </li>
+    </ul>
+
+    <ul class="list-group" v-for="resource in resources" :key="resource.item">
+      <li class="list-group-item px-0 border-0 d-flex justify-content-between">
+        <span>{{ resource.item }}</span>
+        <span>{{ resource.value }} {{ resource.unit }}</span>
       </li>
     </ul>
   </Widget>
@@ -51,7 +58,7 @@ export default {
     }
   },
   computed: {
-    saved: function () {
+    animals() {
       let animals = this.data
       animals = _.map(animals, (animal) =>
         _.assign(animal, {
@@ -64,6 +71,25 @@ export default {
       )
       animals = _.filter(animals, (animal) => animal.value !== '0')
       return animals
+    },
+    resources() {
+      let resources = [
+        { item: 'Water', unit: 'l', value: 1519823 },
+        { item: 'Grain', unit: 'kg', value: 6607 },
+        { item: 'Forest', unit: 'sq. m', value: 1022 },
+        { item: 'CO2', unit: 'kg', value: 3322 },
+      ]
+      resources = _.map(resources, (resource) =>
+        _.assign(resource, {
+          value: (resource.value * this.years).toLocaleString(),
+        }),
+      )
+      resources = _.orderBy(resources, 'value', 'desc')
+      resources = _.map(resources, (resource) =>
+        _.assign(resource, { value: resource.value.toLocaleString() }),
+      )
+      resources = _.filter(resources, (resource) => resource.value !== '0')
+      return resources
     },
   },
   watch: {
