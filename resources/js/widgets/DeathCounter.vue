51efa0bd13003445960a525201dd199d0d034d25
bake <bake@192k.pw>
2020-09-04 16:44:10 +0200

    Fixed number of columns on phones

--- resources/js/widgets/DeathCounter.vue
+++ resources/js/widgets/DeathCounter.vue
@@ -1,7 +1,7 @@
 <template>
   <Widget :name="name" :error="data.error">
     <div class="row">
-      <div class="col" v-for="animals in chunks">
+      <div class="col-6 col-sm-3" v-for="animals in chunks">
         <ul class="list-group" v-for="animal in animals" :key="animal.name">
           <li class="list-group-item px-0 border-0">
             <img
