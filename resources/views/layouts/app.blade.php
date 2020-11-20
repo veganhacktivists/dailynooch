84b3ca3b762458571512bf92f1f4c3c8d512c5bd
David van Beveren <admin@veganhacktivists.org>
2020-11-20 23:53:13 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,13 +15,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SWLZE2TQ7N"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-9STKBLEQXX"></script>
 	<script>
 	  window.dataLayer = window.dataLayer || [];
 	  function gtag(){dataLayer.push(arguments);}
 	  gtag('js', new Date());
 
-	  gtag('config', 'G-SWLZE2TQ7N');
+	  gtag('config', 'G-9STKBLEQXX');
 	</script>
 	
 	<!-- Favicon -->
