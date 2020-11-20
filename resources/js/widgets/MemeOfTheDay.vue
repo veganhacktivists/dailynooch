5af0b336097dbf5a6ab62e6d722097ae43821b5e
Jai Jordan <jai@jaijordan.com>
2020-11-20 00:19:06 +0000

    Meme of the day widget.

---
+++ resources/js/widgets/MemeOfTheDay.vue
@@ -0,0 +1,47 @@
+<template>
+  <Widget :name="name" :error="data.error">
+    <figure>
+      <img class="image" :src="data.imageUrl" />
+    </figure>
+    <ShareUs :title="shareTitle"></ShareUs>
+  </Widget>
+</template>
+
+<script>
+import Widget from '../components/Widget'
+import ShareUs from '../components/ShareUs'
+
+export default {
+  name: 'meme-of-the-day',
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    Widget,
+    ShareUs
+  },
+  computed: {
+    shareTitle() {
+      return `${this.data.imageUrl} - find more vegan memes at ${window.location.protocol + '//' + window.location.hostname}!`
+    }
+  },
+  methods: {
+    share() {
+      alert('not yet implemented')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.image {
+  width: 100%;
+}
+</style>
