6401805fd89dad65aa0474506fb2f573c0b3553b
David van Beveren <admin@veganhacktivists.org>
2020-11-21 09:41:52 +0800

    Map ARM widget english, don't load side panel

--- resources/js/widgets/AnimalRightsMap.vue
+++ resources/js/widgets/AnimalRightsMap.vue
@@ -1,7 +1,7 @@
 <template>
   <Widget :name="name" :error="data.error">
     <figure>
-      <iframe width="100%" height="500px" frameborder="0" allowfullscreen="" src="//umap.openstreetmap.fr/de/map/animal-rights-map_487135?scaleControl=false&amp;miniMap=false&amp;scrollWheelZoom=true&amp;zoomControl=true&amp;allowEdit=false&amp;moreControl=true&amp;searchControl=true&amp;tilelayersControl=false&amp;embedControl=null&amp;datalayersControl=expanded&amp;onLoadPanel=caption&amp;captionBar=false&amp;fullscreenControl=false&amp;locateControl=true&amp;measureControl=false" style="overflow:hidden;height:300px;width:100%"></iframe>
+      <iframe width="100%" height="500px" frameborder="0" allowfullscreen="" src="//umap.openstreetmap.fr/en/map/animal-rights-map_487135?scaleControl=false&amp;miniMap=false&amp;scrollWheelZoom=true&amp;zoomControl=true&amp;allowEdit=false&amp;moreControl=true&amp;searchControl=true&amp;tilelayersControl=false&amp;embedControl=null&amp;captionBar=false&amp;fullscreenControl=false&amp;locateControl=true&amp;measureControl=false" style="overflow:hidden;height:300px;width:100%"></iframe>
     </figure>	
       <a href="https://animalrightsmap.org/" target="_blank"><button class="btn btn-primary">View the full map!</button></a>
 
