a4812d9faa4d58e6ce4cce740fc04fb1c44571b8
David van Beveren <admin@veganhacktivists.org>
2020-11-21 09:35:59 +0800

    Update AnimalRightsMap.php

--- app/Widgets/AnimalRightsMap.php
+++ app/Widgets/AnimalRightsMap.php
@@ -9,4 +9,24 @@ class DocOfTheMonth extends AbstractWidget
     protected $name = 'Animal Rights Map';
     protected $type = 'animal-rights-map';
     protected $description = 'Animal Rights Map';
+
+    protected const DOC_DIR = 'img/doc';
+
+    public function getTtl(): int
+    {
+        return 24 * 60;
+    }
+
+    public function getData(): array
+    {
+        return [
+            'imageUrl' => $this->getRandomDoc()
+        ];
+    }
+
+    private function getRandomDoc(): String
+    {
+        $doc = File::files(public_path(self::DOC_DIR));
+        return self::DOC_DIR.'/'.$doc[array_rand($doc)]->getFilename();
+    }
 }
